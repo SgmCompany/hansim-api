4035826898962b14a3d91a0f7d50d1dcf5d712d1
jnhro <jnhro1@gmail.com>
2025-10-27 18:48:32 +0900

    init

---
+++ src/main/java/com/sgm/hansimapi/HansimApiApplication.java
@@ -0,0 +1,13 @@
+package com.sgm.hansimapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HansimApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HansimApiApplication.class, args);
+	}
+
+}
